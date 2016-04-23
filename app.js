9a501280349151552bdbf859f5cd8537f0f23915
Arvin Rimorin <rayarvin@veb.co.nz>
2016-04-23 11:31:02 +0800

    webhook

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 "use strict";
 
-const http = require('http');
-const url = require('url');
-const server = http.createServer((request, response) => {
-  let url_parts = url.parse(request.url, true)
-  console.dir(url_parts.query);
-  if (url_parts.query['hub.verify_token'] === 'a-stray-dog-starved-for-dreams-howls-tonight') {
-    response.write(url_parts.query['hub.challenge']);
-  }
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-  response.end();
+const router = express.Router();
+
+router.use((request, response, next) => {
+  console.log(request.method, request.url);
+  next();
 });
 
-server.listen(5050);
+router.get('/message_deliveries', (request, response) => {
+  const url_parts = url.parse(request.url, true);
+  if (url_parts.query['hub.verify_token'] === 'a-stray-dog-starved-for-dreams-howls-tonight') {
+    response.send(url_parts.query['hub.challenge']);
+  }
+});
+
+app.post('/webhook/', bodyParser.json(), (req, res) => {
+  const messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      const text = event.message.text;
+      console.dir(text);
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
+app.use('/', router);
+
+app.listen(5050);
 console.log("Server is listening");
