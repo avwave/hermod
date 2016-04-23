a3e762e7ea5d0df7b0c4ce751382d217e8b1a679
Arvin Rimorin <avwave@staging.veb.nz>
2016-04-22 22:34:37 -0400

    verification

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+"use strict";
+
 const http = require('http');
 const url = require('url');
 const server = http.createServer((request, response) => {
-  let url_parts = url.parse(request.url)
-  console.dir(url_parts);
-  if (request.query['hub.verify_token'] === 'a-stray-dog-starved-for-dreams-howls-tonight') {
-    response.write(req.query['hub.challenge']);
+  let url_parts = url.parse(request.url, true)
+  console.dir(url_parts.query);
+  if (url_parts.query['hub.verify_token'] === 'a-stray-dog-starved-for-dreams-howls-tonight') {
+    response.write(url_parts.query['hub.challenge']);
   }
 
   response.end();
